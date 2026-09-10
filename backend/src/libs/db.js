62b424a9654d946e098c404ab735bad64316922d
giabao1340 <luonggiabao060904@gmail.com>
2026-09-10 21:00:30 +0700

    cdn

--- backend/src/libs/db.js
+++ backend/src/libs/db.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 export const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_CONNECTIONSTRING);
