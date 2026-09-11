da96863e71610137ef238340b08698dd8162548b
giabao1340 <luonggiabao060904@gmail.com>
2026-09-11 13:37:03 +0700

    fix bug

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,6 +1,4 @@
 import { SignupForm } from "@/components/auth/signup-form";
-import React from "react";
-
 const SignUpPage = () => {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center p-6 justify-center md:p-10 z-0 absolute inset-0 bg-gradient-purple">
